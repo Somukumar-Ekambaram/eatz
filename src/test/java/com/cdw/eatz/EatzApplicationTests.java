838588ae4a92bbe38a48a293a82ebbbebbf9278c
SomukumarE <115210502+Somukumar-Ekambaram@users.noreply.github.com>
2023-05-21 10:20:57 +0530

    EATZ-Project Created

---
+++ src/test/java/com/cdw/eatz/EatzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdw.eatz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
